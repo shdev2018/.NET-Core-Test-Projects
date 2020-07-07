78e364b14b8dccf31992494a88a64961a7061c4d
shdev2018 <shaunhall.dev@gmail.com>
2020-07-07 16:56:39 +0100

    Addition of Book List project to GitHub repo.

---
+++ BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult>OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Book.Add(Book);
+            await _db.SaveChangesAsync();
+            Message = "Book created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var BookFromDb = await _db.Book.FindAsync(id);
+
+            if(BookFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Book.Remove(BookFromDb);
+            await _db.SaveChangesAsync();
+            Message = "Book has been deleted successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
